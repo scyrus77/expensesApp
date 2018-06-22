48c72447e9e88653803f62c309d8dc07a0ff9a9a
Steve Cyrus <scyrus77@gmail.com>
2018-06-22 12:44:35 -0600

    Completed ExpenseForm Test Part 2.

--- src/tests/components/ExpenseForm.test.js
+++ src/tests/components/ExpenseForm.test.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { shallow } from 'enzyme';
+import  moment from 'moment';
 import ExpenseForm from '../../components/ExpenseForm';
 import expenses from '../fixtures/expenses';
 
@@ -11,5 +12,65 @@ test('should render ExpenseForm correctly', () => {
 test('should render ExpenseForm with expense data', () => {
   const wrapper = shallow(<ExpenseForm expense={ expenses[0] } />);
   expect(wrapper).toMatchSnapshot();
+});
+
+test('should render error for invalid form submission', () => {
+  const wrapper = shallow(<ExpenseForm />);
+  expect(wrapper).toMatchSnapshot();
+  wrapper.find('form').simulate('submit', { preventDefault: () => {} });
+  expect(wrapper.state('error').length).toBeGreaterThan(0);
+  expect(wrapper).toMatchSnapshot();
+});
+
+test('should set description on input change', () => {
+  const newDescription = 'New description';
+  const wrapper = shallow(<ExpenseForm />);
+  wrapper.find('input').at(0).simulate('change', { target: { value: newDescription} });
+  expect(wrapper.state('description')).toBe(newDescription);
+});
+
+test('should set note on textarea change', () => {
+  const newNote = 'New note';
+  const wrapper = shallow(<ExpenseForm />);
+  wrapper.find('textarea').simulate('change', { target: { value: newNote} });
+  expect(wrapper.state('note')).toBe(newNote);
+});
+
+test('should set amount if valid input', () => {
+  const newAmount = '23.50';
+  const wrapper = shallow(<ExpenseForm />);
+  wrapper.find('input').at(1).simulate('change', { target: { value: newAmount} });
+  expect(wrapper.state('amount')).toBe(newAmount);
+});
+
+test('should not set amount if invalid input', () => {
+  const newAmount = '12.122';
+  const wrapper = shallow(<ExpenseForm />);
+  wrapper.find('input').at(1).simulate('change', { target: { value: newAmount} });
+  expect(wrapper.state('amount')).toBe('');
+});
+
+test('should call onSubmit prop for valid form submission', () => {
+  const onSubmitSpy = jest.fn();
+  const wrapper = shallow(<ExpenseForm expense={ expenses[0] } onSubmit={onSubmitSpy}/>);
+  wrapper.find('form').simulate('submit', { preventDefault: () => {} });
+  expect(wrapper.state('error')).toBe('');
+  const testData = {...expenses[0]};
+  delete testData.id;
+  expect(onSubmitSpy).toHaveBeenLastCalledWith(testData);
+});
+
+test('should set new date on date change', () => {
+  const now = moment();
+  const wrapper = shallow(<ExpenseForm/>);
+  wrapper.find('SingleDatePicker').prop('onDateChange')(now);
+  expect(wrapper.state('createdAt')).toBe(now);
+});
+
+test('should set new date on focus change', () => {
+  const focused = true;
+  const wrapper = shallow(<ExpenseForm/>);
+  wrapper.find('SingleDatePicker').prop('onFocusChange')({focused});
+  expect(wrapper.state('calendarFocused')).toBe(focused);
 });
 
