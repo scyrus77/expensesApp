ccc94994594dd29d45032327c9a535470e9c34c9
Steve Cyrus <scyrus77@gmail.com>
2018-06-26 17:23:41 -0600

    Updates for devDependencies and added /dist folder.

--- webpack.config.js
+++ webpack.config.js
@@ -8,8 +8,7 @@ module.exports = (env) => {
   return {
     entry: './src/app.js',
     output: {
-      // path: path.resolve(__dirname, 'public'),
-      path: path.join(__dirname, 'public'),
+      path: path.join(__dirname, 'public', 'dist'),
       filename: "bundle.js"
     },
     module: {
@@ -46,7 +45,8 @@ module.exports = (env) => {
     devtool: isProduction ? 'source-map' : 'inline-source-map',
     devServer: {
       contentBase: path.join(__dirname, 'public'),
-      historyApiFallback: true
+      historyApiFallback: true,
+      publicPath: '/dist/'
     }
   };
 
